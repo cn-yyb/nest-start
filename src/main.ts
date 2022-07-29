a92a476deac9b2a50d0774fcd6660e5e8bab048c
zhongqing <sdpzhong@163.com>
2022-07-29 10:21:47 +0800

    fix: remove js file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORT = 8888;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT, () => {
+    console.log(`The server has been runing at http://127.0.0.1:${PORT}`);
+  });
 }
 bootstrap();
